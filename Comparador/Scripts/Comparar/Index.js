2de10e57653a61189342c6c12f542f306999f3d4
Gustavo Toshi Komura <gustavo.toshi.k@gmail.com>
2019-09-22 21:47:16 -0300

    Melhoria no comparar

---
+++ Comparador/Scripts/Comparar/Index.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $('#Comparar').click(function () {
+        $('#produto1').load("/Produto/Detalhe/" + $('#SelectedProduto1').children("option:selected").val());
+        $('#produto2').load("/Produto/Detalhe/" + $('#SelectedProduto2').children("option:selected").val());
+    });
+});
